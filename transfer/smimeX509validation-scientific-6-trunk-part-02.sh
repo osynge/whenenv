aadf7416b240705192a9f49e2f88472ddd74d8b3
Owen Synge <owen.synge@desy.de>
2013-06-09 13:48:53 +0200

    adding smimeX509validation-scientific-6-trunk

---
+++ transfer/smimeX509validation-scientific-6-trunk-part-02.sh
@@ -0,0 +1,44 @@
+export CROOT_DIR="/root/sl6"
+#chroot ${CROOT_DIR}
+CHROOT_SCRIPT=${CROOT_DIR}/script
+cat > ${CHROOT_SCRIPT} <<-EOF
+#!/bin/bash
+id
+hostname -f
+yum install git \
+  org-desy-grid-virt-sort-release \
+  rpmbuild \
+  python-setuptools \
+  rpm-build \
+  m2crypto \
+  -y
+GITLOCATION="git://github.com/hepix-virtualisation/smimeX509validation.git"
+rm -rf build
+git clone \${GITLOCATION} build
+cd build
+
+python setup.py sdist
+for src in \$(ls dist/*.tar\.gz | grep -v \.src\.tar\.gz )
+do
+newname=\$( echo \${src} | sed -e "s/tar\.gz/rc${BUILD_NUMBER}\.src\.tar\.gz/")
+mv \$src \$newname
+done
+python setup.py bdist_rpm \
+    --release rc${BUILD_NUMBER} \
+    --requires  "m2crypto fetch-crl"
+python setup.py bdist
+architecture=\$(arch)
+for src in \$(ls dist/*.tar.gz | grep \$architecture )
+do
+newname=\$( echo \${src} | sed -e "s/tar\.gz/rc${BUILD_NUMBER}\.bin\.tar\.gz/")
+mv \$src \$newname
+done
+EOF
+echo xx
+cat ${CHROOT_SCRIPT}
+echo xx
+chroot ${CROOT_DIR} /bin/bash /script
+rm -rf build
+mv ${CROOT_DIR}/build build
+rm -f artifacts.tgz
+tar -zcvf artifacts.tgz build/dist
