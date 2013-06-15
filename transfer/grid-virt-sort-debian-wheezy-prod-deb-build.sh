ac3894c3d85a222ebaec8acd96cdc7449b9ffc92
Owen Synge <owen.synge@jaysnest.de>
2013-06-15 20:24:30 +0200

    moving th edirs around

--- transfer/grid-virt-sort-debian-wheezy-prod-deb-build.sh
+++ transfer/grid-virt-sort-debian-wheezy-prod-deb-build.sh
@@ -3,13 +3,14 @@ set -x
 chroot chroot
 id
 hostname -fS
+dirStart=`pwd`
 rm -f artifacts.tgz *.dsc *.deb 
 GITLOCATION="git://github.com/osynge/deb-grid-virt-sort.git"
 rm -rf build
-cmd="git clone ${GITLOCATION} build"
+cmd="git clone ${GITLOCATION} build/gitclone"
 echo $cmd
 $cmd
-cd build
+cd build/gitclone
 git checkout upstream
 git pull
 git checkout pristine-tar
@@ -17,6 +18,7 @@ git pull
 git checkout master
 git pull
 echo `git-buildpackage`
+cd ${dirStart}
 rm -f artifacts.tgz
-tar -zcvf artifacts.tgz *.deb *.dsc
+tar -zcvf artifacts.tgz build/*.deb *.dsc
 
