011bf63d97f646f2268bc8b13393f1aa2be39df3
Owen Synge <owen.synge@jaysnest.de>
2013-06-15 20:52:52 +0200

    some modifications

--- transfer/grid-virt-sort-debian-wheezy-prod-deb-export.sh
+++ transfer/grid-virt-sort-debian-wheezy-prod-deb-export.sh
@@ -7,25 +7,9 @@ PLATFORM="x86_64"
 FLAVOR="debian"
 VERSION="7.0"
 mkdir -p ${ROOTDIR}
-dir_tgz="${ROOTDIR}/${RELEASE_TYPE}/source/${FLAVOR}/${VERSION}/tgz/"
-dir_btgz="${ROOTDIR}/${RELEASE_TYPE}/${PLATFORM}/${FLAVOR}/${VERSION}/tgz/"
-dir_srpm="${ROOTDIR}/${RELEASE_TYPE}/${PLATFORM}/${FLAVOR}/${VERSION}/srpm/"
-dir_rpm="${ROOTDIR}/${RELEASE_TYPE}/${PLATFORM}/${FLAVOR}/${VERSION}/rpm/"
-mkdir -p ${dir_tgz}
-mkdir -p ${dir_btgz}
-#mkdir -p ${dir_srpm}
-#mkdir -p ${dir_rpm}
-ls build/*
-/usr/bin/rsync -v --ignore-existing build/dist/*.src.tar.gz \
-    ${dir_tgz}
-rm -rf build/dist/*src.tar.gz
-/usr/bin/rsync -v --ignore-existing build/dist/*.bin.tar.gz \
-    ${dir_btgz}
-rm -rf build/dist/*bin.tar.gz
+dir_deb="${ROOTDIR}/${RELEASE_TYPE}/source/${FLAVOR}/${VERSION}/packages/"
+mkdir -p ${dir_deb}
 
-#/usr/bin/rsync -v --ignore-existing build/dist/*.src.rpm \
-#    ${dir_srpm}
-#rm -rf build/dist/*.src.rpm
-#/usr/bin/rsync -v --ignore-existing build/dist/*.rpm \
-#    ${dir_rpm}
-#rm -rf build/dist/*.rpm
+ls build/*
+/usr/bin/rsync -v --ignore-existing build/ \
+    ${dir_deb}
