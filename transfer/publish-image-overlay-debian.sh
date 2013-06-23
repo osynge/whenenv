801977d27942f5533c39622ed60c0a06821e5019
Owen Synge <owen.synge@jaysnest.de>
2013-06-24 00:09:23 +0200

    adding first version of publish-image-overlay

---
+++ transfer/publish-image-overlay-debian.sh
@@ -0,0 +1,20 @@
+rm -rf vm_overlays
+dirStart=`pwd`
+git clone git://git.fritz.box/vm_overlays.git vm_overlays
+cd vm_overlays
+
+branches=`git branch  -a | grep remote | sed -e 's/.*remotes\/origin\///'`
+for this in $branches ; do
+cd ${dirStart}/vm_overlays
+git checkout $this
+cd content
+find . -print |cpio -o -Hnewc |bzip2 -9 -z -q -f > ${dirStart}/${this}.cpio.bz2
+done
+
+
+su exporter -c"vmcaster \
+   --database sqlite:////var/lib/vmcaster/local.db \
+   --upload-image debian-7-yokel-prod.cpio.bz2 \
+   --select-image 333fe448-28c9-44da-a9cb-402818d433bb \
+   --verbose    --verbose"
+rm -rf sl6.cpio.bz2
