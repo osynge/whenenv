cb03a53f48b9e8f53f8391f46fcf52573015e067
Owen Synge <owen.synge@jaysnest.de>
2013-06-24 00:32:03 +0200

    explisit

--- transfer/publish-image-overlay-debian.sh
+++ transfer/publish-image-overlay-debian.sh
@@ -4,7 +4,12 @@ dirStart=`pwd`
 git clone git://git.fritz.box/vm_overlays.git vm_overlays
 cd vm_overlays
 
-branches=`git branch  -a | grep -v  "\->" | grep remote | sed -e 's/.*remotes\/origin\///'`
+branches="debian-7-yokel-prod sl-6-yokel-testing-prod sl-6-yokel-testing-trunk"
+
+for this in $branches ; do
+rm ${this}.cpio.bz2
+done
+
 for this in $branches ; do
 cd ${dirStart}/vm_overlays
 git checkout -b origin/$this
@@ -19,4 +24,19 @@ su exporter -c"vmcaster \
    --upload-image debian-7-yokel-prod.cpio.bz2 \
    --select-image 333fe448-28c9-44da-a9cb-402818d433bb \
    --verbose    --verbose"
-rm -rf sl6.cpio.bz2
+
+su exporter -c"vmcaster \
+   --database sqlite:////var/lib/vmcaster/local.db \
+   --upload-image sl-6-yokel-testing-prod.cpio.bz2 \
+   --select-image 6b9384e5-5923-4d47-aaab-0fde0c52f8b8 \
+   --verbose    --verbose"
+
+su exporter -c"vmcaster \
+   --database sqlite:////var/lib/vmcaster/local.db \
+   --upload-image sl-6-yokel-testing-trunk.cpio.bz2 \
+   --select-image bba0d5e8-3c70-49e0-8479-a42278aea120 \
+   --verbose    --verbose"
+
+for this in $branches ; do
+rm ${this}.cpio.bz2
+done
