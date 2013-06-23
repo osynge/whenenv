815d6b371ca4042c7e79c21835077cafcfb389d6
Owen Synge <owen.synge@jaysnest.de>
2013-06-23 20:04:44 +0200

    adding/chroot-imagelist-update

---
+++ transfer/chroot-imagelist-update.sh
@@ -0,0 +1,6 @@
+su exporter -c"vmcaster \
+   --database sqlite:////var/lib/vmcaster/local.db \
+   --select-imagelist ab1754cd-7d59-4851-c333-c96cb6545aaa \
+   --upload-imagelist \
+   --verbose \
+   --verbose"
