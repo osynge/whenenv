935c5d99a7c06001208c1042d5591884a610d0eb
Owen Synge <owen.synge@jaysnest.de>
2013-06-23 19:13:33 +0200

    addding vmcaster-scientific-6-prod

---
+++ transfer/vmcaster-scientific-6-prod-part-02.sh
@@ -0,0 +1,35 @@
+export CROOT_DIR="/root/sl6"
+
+
+
+#chroot ${CROOT_DIR}
+CHROOT_SCRIPT=${CROOT_DIR}/script
+cat > ${CHROOT_SCRIPT} <<-EOF
+#!/bin/bash
+id
+hostname -f
+
+hostname -f
+yum clean all
+yum install git  org_desy_grid_virt_sort_release \
+  rpmbuild \
+  python-setuptools \
+  rpm-build \
+  -y
+GITLOCATION="git://github.com/hepix-virtualisation/vmcaster.git"
+rm -rf build
+git clone \${GITLOCATION} build
+cd build
+latest_tag=\$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
+git checkout \${latest_tag}
+python setup.py bdist_rpm \
+    --requires  "python-sqlalchemy m2crypto python-magic"
+EOF
+echo xx
+cat ${CHROOT_SCRIPT}
+echo xx
+chroot ${CROOT_DIR} /bin/bash /script
+rm -rf build
+mv ${CROOT_DIR}/build build
+rm -f artifacts.tgz
+tar -zcvf artifacts.tgz build/dist
