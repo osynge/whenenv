1daf21d90d0b44194947eb19cf8cf0baf818cdb3
Owen Synge <owen.synge@jaysnest.de>
2013-06-23 19:39:47 +0200

    added hepixvmitrust-debian-wheezy-prod

---
+++ transfer/hepixvmitrust-debian-wheezy-prod-part-03.sh
@@ -0,0 +1,4 @@
+chroot chroot
+apt-get install git     python     rpm     make       python-simplejson        python-m2crypto     python-setuptools     -y
+
+echo shoudl have org-desy-grid-virt-sort-release
