2284710e4dc014d8506905f559e342380d086ed8
Owen Synge <owen.synge@jaysnest.de>
2013-06-23 19:23:39 +0200

    adding hepixvmitrust-scientific-6-trunk

---
+++ transfer/hepixvmitrust-scientific-6-trunk.sh
@@ -0,0 +1,47 @@
+export CROOT_DIR="/root/sl6"
+python /root/chrootbuilder \
+  --input \
+  /var/cache/vmcatcher/endorsed/aa42ca85-179b-4873-b12e-32d549bf02b6 \
+  --dest  /root/sl6 \
+  --overlay /root/overlay.cpio.bz2 \
+  --build
+#chroot ${CROOT_DIR}
+CHROOT_SCRIPT=${CROOT_DIR}/script
+cat > ${CHROOT_SCRIPT} <<-EOF
+#!/bin/bash
+yum install git \
+  org-desy-grid-virt-sort-release \
+  rpmbuild \
+  python-setuptools \
+  rpm-build \
+  m2crypto \
+  -y
+GITLOCATION="git://github.com/hepix-virtualisation/hepixvmitrust.git"
+rm -rf build
+git clone \${GITLOCATION} build
+cd build
+python setup.py sdist
+for src in \$(ls dist/*.tar\.gz | grep -v \.src\.tar\.gz )
+do
+newname=\$( echo \${src} | sed -e "s/tar\.gz/rc${BUILD_NUMBER}\.src\.tar\.gz/")
+mv \$src \$newname
+done
+python setup.py bdist_rpm \
+    --release rc${BUILD_NUMBER}
+python setup.py bdist
+architecture=\$(arch)
+for src in \$(ls dist/*.tar.gz | grep \$architecture )
+do
+newname=\$( echo \${src} | sed -e "s/tar\.gz/rc${BUILD_NUMBER}\.bin\.tar\.gz/")
+mv \$src \$newname
+done
+
+EOF
+echo xx
+cat ${CHROOT_SCRIPT}
+echo xx
+chroot ${CROOT_DIR} /bin/bash /script
+rm -rf build
+mv ${CROOT_DIR}/build build
+rm -f artifacts.tgz
+tar -zcvf artifacts.tgz build/dist
