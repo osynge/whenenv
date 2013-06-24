28b88bb8954108227801749a908318eff50b711d
Owen Synge <owen.synge@jaysnest.de>
2013-06-24 14:20:59 +0200

    making things cleaner

--- transfer/dirvacua-scientific-6-prod.sh
+++ transfer/dirvacua-scientific-6-prod.sh
@@ -1,12 +1,5 @@
 set -x
 export CROOT_DIR="/root/sl6"
-python /root/chrootbuilder \
---input \
-/var/cache/vmcatcher/endorsed/aa42ca85-179b-4873-b12e-32d549bf02b6 \
---dest ${CROOT_DIR} \
---overlay  /var/cache/vmcatcher/endorsed/6b9384e5-5923-4d47-aaab-0fde0c52f8b8 \
---build
-#chroot ${CROOT_DIR}
 CHROOT_SCRIPT=${CROOT_DIR}/script
 cat > ${CHROOT_SCRIPT} <<-EOF
 #!/bin/bash
