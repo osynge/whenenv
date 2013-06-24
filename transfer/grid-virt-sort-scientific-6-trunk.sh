fdc4df105ed8d78b4a6c2ef5977ecde781e7c5c5
Owen Synge <owen.synge@jaysnest.de>
2013-06-24 13:48:43 +0200

    cleaning here

--- transfer/grid-virt-sort-scientific-6-trunk.sh
+++ transfer/grid-virt-sort-scientific-6-trunk.sh
@@ -1,12 +1,5 @@
 export CROOT_DIR="/root/sl6"
-
-python /root/chrootbuilder \
---input \
-/var/cache/vmcatcher/endorsed/aa42ca85-179b-4873-b12e-32d549bf02b6 \
---dest  /root/sl6 \
---overlay /root/overlay.cpio.bz2 \
---build
-#chroot ${CROOT_DIR}
+set -x
 CHROOT_SCRIPT=${CROOT_DIR}/script
 cat > ${CHROOT_SCRIPT} <<-EOF
 #!/bin/bash
