1b2f0b91cc4b0bce458a13204b2175a35f3d2ac2
Owen Synge <owen.synge@jaysnest.de>
2013-06-29 01:11:49 +0200

    docbook-production.sh

--- transfer/docbook-production.sh
+++ transfer/docbook-production.sh
@@ -1,1 +1,13 @@
-echo xx
+hostname -f
+
+SVNLOCATION=&quot;https://svnsrv.desy.de/public/grid-virt/org.hepix.virtulization.docs/trunk&quot;
+#TAG=`svn ls ${SVNLOCATION} | org_desy_grid_virt_sort_release.py | tail -n 1`
+rm -rf build
+#svn co ${SVNLOCATION}/${TAG} build
+svn co ${SVNLOCATION} build
+cd build
+make html
+make pdf
+cd ..
+rm -f artifacts.tgz
+tar -zcvf artifacts.tgz build/*.pdf build/Book
