b95d2a12b74d077c027ebd1c2692d9fcb9441313
Owen Synge <owen.synge@jaysnest.de>
2013-06-29 15:02:17 +0200

    now it shoudl be working

--- transfer/docbook-production.sh
+++ transfer/docbook-production.sh
@@ -1,13 +1,21 @@
 hostname -f
 
+
+
 SVNLOCATION="https://svnsrv.desy.de/public/grid-virt/org.hepix.virtulization.docs/trunk"
 #TAG=`svn ls ${SVNLOCATION} | org_desy_grid_virt_sort_release.py | tail -n 1`
 rm -rf build
 #svn co ${SVNLOCATION}/${TAG} build
+pwd
 svn co ${SVNLOCATION} build
+pwd
+#exit 1
 cd build
 make html
 make pdf
 cd ..
 rm -f artifacts.tgz
 tar -zcvf artifacts.tgz build/*.pdf build/Book
+
+frog="foo"
+export frog
