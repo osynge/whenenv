c4a49ba83defef3e72e9c7037e5fb2e6ccf3d1da
Owen Synge <owen.synge@jaysnest.de>
2013-06-30 19:23:44 +0200

    moving to new chrootbuilder

--- transfer/chroot-scientific-6-buildup-prod.sh
+++ transfer/chroot-scientific-6-buildup-prod.sh
@@ -1,6 +1,6 @@
 set -x
 export CROOT_DIR="/root/sl6"
-python /root/chrootbuilder \
+/usr/bin/chrootbuilder \
 --input \
 /var/cache/vmcatcher/endorsed/aa42ca85-179b-4873-b12e-32d549bf02b6 \
 --dest ${CROOT_DIR} \
--- transfer/chrootbuilder-scientific-6-prod.sh
+++ transfer/chrootbuilder-scientific-6-prod.sh
@@ -20,8 +20,8 @@ GITLOCATION="git://git.fritz.box/chrootbuilder.git"
 rm -rf build
 git clone \${GITLOCATION} build
 cd build
-latest_tag=\$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
-git checkout \${latest_tag}
+#latest_tag=\$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
+#git checkout \${latest_tag}
 python setup.py bdist_rpm \
     --requires  "python"
 EOF
