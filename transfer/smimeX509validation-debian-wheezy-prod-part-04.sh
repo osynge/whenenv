0c91940badc4f48b8d850949208d5deedc12d1ec
Owen Synge <owen.synge@jaysnest.de>
2013-06-30 13:50:52 +0200

    migrating to new chroot

--- transfer/smimeX509validation-debian-wheezy-prod-part-04.sh
+++ transfer/smimeX509validation-debian-wheezy-prod-part-04.sh
@@ -6,8 +6,8 @@ GITLOCATION="git://github.com/hepix-virtualisation/smimeX509validation.git"
 rm -rf build
 git clone ${GITLOCATION} build
 cd build
-latest_tag=$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
-#latest_tag=`git tag | tail -n 1`
+#latest_tag=$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
+latest_tag=`git tag | tail -n 1`
 git checkout ${latest_tag}
 python setup.py sdist
 for src in $(ls dist/*.tar\.gz | grep -v .src.tar.gz )
