d505c1e98b5a69278819b2c90a03df81425ab70b
Owen Synge <owen.synge@jaysnest.de>
2013-07-13 15:26:27 +0200

    start of the process

--- transfer/chroot-scientific-6-teardown.sh
+++ transfer/chroot-scientific-6-teardown.sh
@@ -1,15 +1,3 @@
-function ycheckrc () {
-echo $@
-$@
-rc=$?
-if [ $[${rc}] != 0 ] ; then
-    exit ${rc}
-fi
-}
-ycheckrc mv ${CHROOT}/artifacts.tgz .
-
-
-
 export CROOT_DIR="/root/sl6"
 chrootbuilder \
   --dest  ${CROOT_DIR} \
