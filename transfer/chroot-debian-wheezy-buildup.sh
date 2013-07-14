bd5191b245c03c39f9d0a003b2230ea848361cb8
Owen Synge <owen.synge@jaysnest.de>
2013-07-14 04:15:40 +0200

    made nicer

--- transfer/chroot-debian-wheezy-buildup.sh
+++ transfer/chroot-debian-wheezy-buildup.sh
@@ -1,7 +1,6 @@
 #UUID=aa42ca85-179b-4873-b12e-32d549bf02b6
 UUID=aa7016b0-6508-41d2-bce0-c1724cb3d3e2
 UUID_Overlay=333fe448-28c9-44da-a9cb-402818d433bb
-CHROOT="/workspace/chroot"
 
 python /root/chrootbuilder \
   --input \
@@ -10,5 +9,12 @@ python /root/chrootbuilder \
   --overlay /var/cache/vmcatcher/endorsed/${UUID_Overlay} \
   --build
 
+#UUID=aa42ca85-179b-4873-b12e-32d549bf02b6
+UUID=aa7016b0-6508-41d2-bce0-c1724cb3d3e2
+UUID_Overlay=333fe448-28c9-44da-a9cb-402818d433bb
+CHROOT="/workspace/chroot"
+python /root/chrootbuilder   --input   /var/cache/vmcatcher/endorsed/${UUID}   --dest  ${CHROOT}   --overlay /var/cache/vmcatcher/endorsed/${UUID_Overlay}   --build
 CHROOT="/workspace/document-production/chroot"
 export CHROOT
+echo $CHROOT
+chrootbuilder   --input   /var/cache/vmcatcher/endorsed/${UUID}   --dest  ${CHROOT}   --overlay /var/cache/vmcatcher/endorsed/${UUID_Overlay}   --build
