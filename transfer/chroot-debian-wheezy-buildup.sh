f7fb4eb387962e3ead5415e96e99dd925a81aaf0
Owen Synge <owen.synge@jaysnest.de>
2013-07-14 03:14:18 +0200

    made nicer

--- transfer/chroot-debian-wheezy-buildup.sh
+++ transfer/chroot-debian-wheezy-buildup.sh
@@ -1,10 +1,10 @@
 #UUID=aa42ca85-179b-4873-b12e-32d549bf02b6
 UUID=aa7016b0-6508-41d2-bce0-c1724cb3d3e2
 UUID_Overlay=333fe448-28c9-44da-a9cb-402818d433bb
-CHROOT="/workspace/document-production/chroot"
+CHROOT="/workspace/chroot"
 if [ ! -d ${CHROOT} ]
 then
-mkdir chroot
+mkdir ${CHROOT}
 fi
 
 
