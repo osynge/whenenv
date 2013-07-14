f2a741b1bd13a0f620c562aa5c16c24bff33de19
Owen Synge <owen.synge@jaysnest.de>
2013-07-14 02:33:18 +0200

    made nicer

--- transfer/chroot-scientific-6-buildup-prod.sh
+++ transfer/chroot-scientific-6-buildup-prod.sh
@@ -4,9 +4,7 @@ rm -rf artifacts.tgz
 /usr/bin/chrootbuilder \
 --input \
 /var/cache/vmcatcher/endorsed/aa42ca85-179b-4873-b12e-32d549bf02b6 \
---dest ${CROOT_DIR} \
+--dest ${CHROOT} \
 --overlay  /var/cache/vmcatcher/endorsed/6b9384e5-5923-4d47-aaab-0fde0c52f8b8 \
 --build
-export CROOT_DIR
-CHROOT=$CROOT_DIR
 export CHROOT
