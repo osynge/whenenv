59a349500b267c2e65405568f20d8976178bcb0a
Owen Synge <owen.synge@jaysnest.de>
2013-07-14 18:05:19 +0200

    modernising

--- transfer/chrootbuilder-debian-wheezy-prod.sh
+++ transfer/chrootbuilder-debian-wheezy-prod.sh
@@ -4,5 +4,20 @@ git clone ${GITLOCATION} build
 cd build
 #latest_tag=\$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
 #git checkout \${latest_tag}
-python setup.py bdist_rpm \
-    --requires  "python"
+
+python setup.py sdist
+for src in $(ls dist/*.tar\.gz | grep -v .src.tar.gz )
+do
+  newname=$( echo ${src} | sed -e "s/tar.gz/src.tar.gz/")
+  mv $src $newname
+done
+python setup.py bdist
+architecture=$(arch)
+for src in $(ls dist/*.tar.gz | grep $architecture )
+do
+  newname=`echo ${src} | sed -e "s/tar\.gz/bin\.tar\.gz/"`
+  mv $src $newname
+done
+cd ..
+rm -f artifacts.tgz
+tar -zcvf artifacts.tgz build/dist
