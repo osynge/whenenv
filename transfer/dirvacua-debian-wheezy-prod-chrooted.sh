dbd86966e89ff790a5d8688292ec63db0de5276c
Owen Synge <owen.synge@jaysnest.de>
2013-07-14 13:52:46 +0200

    now tester works

---
+++ transfer/dirvacua-debian-wheezy-prod-chrooted.sh
@@ -0,0 +1,26 @@
+pwd
+id
+hostname -f
+GITLOCATION="https://github.com/osynge/dirvacua.git"
+rm -rf build
+git clone ${GITLOCATION} build
+cd build
+latest_tag=$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
+#latest_tag=`git tag | tail -n 1`
+git checkout ${latest_tag}
+python setup.py sdist
+for src in $(ls dist/*.tar\.gz | grep -v .src.tar.gz )
+do
+  newname=$( echo ${src} | sed -e "s/tar.gz/src.tar.gz/")
+  mv $src $newname
+done
+python setup.py bdist
+architecture=$(arch)
+for src in $(ls dist/*.tar.gz | grep $architecture )
+do
+  newname=`echo ${src} | sed -e "s/tar\.gz/bin\.tar\.gz/"`
+  mv $src $newname
+done
+cd ..
+rm -f artifacts.tgz
+tar -zcvf artifacts.tgz build/dist
