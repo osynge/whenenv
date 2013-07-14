c671cc9cc6d99f689b3df0768e3a6a963a5a2f0b
Owen Synge <owen.synge@jaysnest.de>
2013-07-14 11:55:20 +0200

    well it will bvreak differently now

--- transfer/chroot-debian-wheezy-buildup.sh
+++ transfer/chroot-debian-wheezy-buildup.sh
@@ -3,20 +3,12 @@ UUID="aa7016b0-6508-41d2-bce0-c1724cb3d3e2"
 UUID_Overlay="333fe448-28c9-44da-a9cb-402818d433bb"
 set +e
 echo presetvalue=${CHROOT}
-export CHROOT="/workspace/chroot"
+CHROOT="/workspace/chroot"
+echo presetvalue=${CHROOT}
+export CHROOT
 chrootbuilder \
   --input \
   /var/cache/vmcatcher/endorsed/${UUID} \
   --dest  ${CHROOT} \
   --overlay /var/cache/vmcatcher/endorsed/${UUID_Overlay} \
   --build
-
-#UUID=aa42ca85-179b-4873-b12e-32d549bf02b6
-UUID=aa7016b0-6508-41d2-bce0-c1724cb3d3e2
-UUID_Overlay=333fe448-28c9-44da-a9cb-402818d433bb
-python /root/chrootbuilder   --input   /var/cache/vmcatcher/endorsed/${UUID}   --dest  ${CHROOT}   --overlay /var/cache/vmcatcher/endorsed/${UUID_Overlay}   --build
-CHROOT="/workspace/document-production/chroot"
-export CHROOT=$CHROOT
-echo $CHROOT
-set -e
-chrootbuilder   --input   /var/cache/vmcatcher/endorsed/${UUID}   --dest  ${CHROOT}   --overlay /var/cache/vmcatcher/endorsed/${UUID_Overlay}   --build
