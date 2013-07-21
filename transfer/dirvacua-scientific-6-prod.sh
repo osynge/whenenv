a512aa8a8f5cc0cb47ec3fb5c9adccfbfba8f0bc
Owen Synge <owen.synge@jaysnest.de>
2013-07-21 09:28:58 +0200

    some changes

--- transfer/dirvacua-scientific-6-prod.sh
+++ transfer/dirvacua-scientific-6-prod.sh
@@ -1,7 +1,4 @@
 set -x
-CHROOT_SCRIPT=${CHROOT}/script
-cat > ${CHROOT_SCRIPT} <<-EOF
-#!/bin/bash
 export http_proxy=http://squid:3128
 
 id
@@ -14,32 +11,25 @@ yum install git \
   -y
 GITLOCATION="https://github.com/osynge/dirvacua.git"
 rm -rf build
-git clone \${GITLOCATION} build
+git clone ${GITLOCATION} build
 cd build
-latest_tag=\$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
-git checkout \${latest_tag}
+latest_tag=$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
+git checkout ${latest_tag}
 ls /dev/urandom
 python setup.py sdist
-for src in \$(ls dist/*.tar\.gz | grep -v \.src\.tar\.gz )
+for src in $(ls dist/*.tar\.gz | grep -v \.src\.tar\.gz )
 do
-newname=\$( echo \${src} | sed -e "s/tar\.gz/src\.tar\.gz/")
-mv \$src \$newname
+newname=$( echo ${src} | sed -e "s/tar\.gz/src\.tar\.gz/")
+mv $src $newname
 done
 python setup.py bdist_rpm \
     --requires  "m2crypto python-simplejson python-hashlib python-uuid"
 python setup.py bdist
-architecture=\$(arch)
-for src in \$(ls dist/*.tar.gz | grep \$architecture )
+architecture=$(arch)
+for src in $(ls dist/*.tar.gz | grep \$architecture )
 do
-newname=\$( echo \${src} | sed -e "s/tar\.gz/bin\.tar\.gz/")
-mv \$src \$newname
+newname=$( echo \${src} | sed -e "s/tar\.gz/bin\.tar\.gz/")
+mv $src $newname
 done
-EOF
-echo xx
-cat ${CHROOT_SCRIPT}
-echo xx
-chroot ${CHROOT} /bin/bash /script
-rm -rf build
-mv ${CHROOT}/build build
 rm -f artifacts.tgz
 tar -zcvf artifacts.tgz build/dist
