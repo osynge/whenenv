82e7fcbf632c0fb0199ebbb47f4dea3fdf680004
Owen Synge <owen.synge@jaysnest.de>
2013-07-21 12:19:09 +0200

    now its more modern

--- transfer/vmcatcher-scientific-6-prod.sh
+++ transfer/vmcatcher-scientific-6-prod.sh
@@ -1,31 +1,27 @@
 set -x
 
-#chroot ${CHROOT}
-CHROOT_SCRIPT=${CHROOT}/script
-cat > ${CHROOT_SCRIPT} <<-EOF
-#!/bin/bash
-yum install git  org_desy_grid_virt_sort_release \
-  rpmbuild \
-  python-setuptools \
-  rpm-build \
-  -y
 GITLOCATION="git://github.com/hepix-virtualisation/vmcatcher.git"
 rm -rf build
-git clone \${GITLOCATION} build
+git clone ${GITLOCATION} build
 cd build
 #latest_tag=$(git tag | org_desy_grid_virt_sort_release.py | tail -n 1)
 latest_tag=$(git tag | grep vmcatcher | tail -n 1)
 git checkout ${latest_tag}
+python setup.py sdist
+for src in $(ls dist/*.tar\.gz | grep -v \.src\.tar\.gz )
+do
+newname=$( echo ${src} | sed -e "s/tar\.gz/src\.tar\.gz/")
+mv $src $newname
+done
 python setup.py bdist_rpm \
     --requires  "smimeX509validation hepixvmitrust python-sqlalchemy fetch-crl"
-
-
-EOF
-echo xx
-cat ${CHROOT_SCRIPT}
-echo xx
-chroot ${CHROOT} /bin/bash /script
-rm -rf build
-mv ${CHROOT}/build build
+python setup.py bdist
+architecture=$(arch)
+for src in $(ls dist/*.tar.gz | grep $architecture )
+do
+newname=$( echo ${src} | sed -e "s/tar\.gz/bin\.tar\.gz/")
+mv $src $newname
+done
+cd ..
 rm -f artifacts.tgz
 tar -zcvf artifacts.tgz build/dist
