3af09073caf0d589ec32737aa121bed134a46ff7
Owen Synge <owen.synge@jaysnest.de>
2013-07-21 13:25:10 +0200

    does it now work

--- transfer/chroot-imagelist-update.sh
+++ transfer/chroot-imagelist-update.sh
@@ -1,10 +1,9 @@
 cd /
 su exporter
-#vmcaster \
-#   --database sqlite:////var/lib/vmcaster/local.db \
-#   --select-imagelist ab1754cd-7d59-4851-c333-c96cb6545aaa \
-#   --upload-imagelist \
-#   --verbose \
-#   --verbose
+vmcaster \
+   --database sqlite:////var/lib/vmcaster/local.db \
+   --select-imagelist ab1754cd-7d59-4851-c333-c96cb6545aaa \
+   --upload-imagelist \
+
 exit 0
 
