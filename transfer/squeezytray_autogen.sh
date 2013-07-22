fdadf91872f335633a41e01ba6763abab3f483f8
Owen Synge <owen.synge@jaysnest.de>
2013-07-22 17:05:55 +0200

    include more icons

---
+++ transfer/squeezytray_autogen.sh
@@ -0,0 +1,20 @@
+if [ "X${BUILD_SRC}" == "X" ] ; then
+    echo "BUILD_SRC not defined"
+    exit 1
+fi
+ORIGINALDIR=`pwd`
+cd $BUILD_SRC
+
+sizes="16 22 24 32 48 72 128"
+basedirect="icons"
+for item in `ls icons/*.svg`
+do
+   for size in $sizes
+   do
+       name=$(echo $item | sed -e 's/.*\///' | sed -e 's/\.svg.*$//')
+       cmd="convert  -background transparent  -resize ${size}x${size} $item  ${basedirect}/${name}_${size}x${size}.png"
+       $cmd
+   done
+done
+
+cd $ORIGINALDIR
