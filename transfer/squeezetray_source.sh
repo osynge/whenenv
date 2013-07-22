b40bd9e9e6f0e6e9a4c7520dda650edc511bf644
Owen Synge <owen.synge@jaysnest.de>
2013-07-22 17:10:01 +0200

    now we have a tag

--- transfer/squeezetray_source.sh
+++ transfer/squeezetray_source.sh
@@ -1,6 +1,6 @@
 GIT_SRC="git://github.com/osynge/squeezetray.git"
 GIT_DEST="build"
-GIT_TAG_FILTER="squeezetray-"
+GIT_TAG_FILTER="SqueezeWxTray-"
 export GIT_SRC
 export GIT_DEST
 export GIT_TAG_FILTER
