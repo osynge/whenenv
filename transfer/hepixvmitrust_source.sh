1ab92e788ecf9ea108a6ef60e89aa349e9ea979d
Owen Synge <owen.synge@jaysnest.de>
2013-07-24 20:14:35 +0200

    shodul now build

---
+++ transfer/hepixvmitrust_source.sh
@@ -0,0 +1,7 @@
+GIT_SRC="git://github.com/hepix-virtualisation/hepixvmitrust.git"
+GIT_DEST="build"
+GIT_TAG_FILTER="hepixvmitrust-"
+export GIT_SRC
+export GIT_DEST
+export GIT_TAG_FILTER
+echo billll
