7b7ebc217d8df78518b1cabf85b39d32e06b94aa
Owen Synge <owen.synge@jaysnest.de>
2013-07-24 21:29:17 +0200

    adding vmcatcher_eventHndlExpl_ON

---
+++ transfer/vmcatcher_eventHndlExpl_ON_source.sh
@@ -0,0 +1,8 @@
+GIT_SRC="git://github.com/grid-admin/vmcatcher_eventHndlExpl_ON.git"
+GIT_DEST="build"
+GIT_TAG_FILTER="vmcatcher_eventHndlExpl_ON-"
+RPM_DEPENDS="vmcatcher"
+export GIT_SRC
+export GIT_DEST
+export GIT_TAG_FILTER
+export RPM_DEPENDS
