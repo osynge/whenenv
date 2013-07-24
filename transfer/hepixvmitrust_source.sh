842af43705d0c3059ae56b719d1247769b72234e
Owen Synge <owen.synge@jaysnest.de>
2013-07-24 20:49:43 +0200

    RPM_DEPENDS

--- transfer/hepixvmitrust_source.sh
+++ transfer/hepixvmitrust_source.sh
@@ -5,3 +5,4 @@ RPM_DEPENDS="m2crypto"
 export GIT_SRC
 export GIT_DEST
 export GIT_TAG_FILTER
+export RPM_DEPENDS
--- transfer/squeezetray_source.sh
+++ transfer/squeezetray_source.sh
@@ -1,7 +1,8 @@
 GIT_SRC="git://github.com/osynge/squeezetray.git"
 GIT_DEST="build"
 GIT_TAG_FILTER="SqueezeWxTray-"
+RPM_DEPENDS="wxPython"
 export GIT_SRC
 export GIT_DEST
 export GIT_TAG_FILTER
-
+export RPM_DEPENDS
