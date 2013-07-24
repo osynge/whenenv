c8f2027943d3fde88366736ea2614b8bff645603
Owen Synge <owen.synge@jaysnest.de>
2013-07-24 22:56:18 +0200

    make it non ambigious

---
+++ transfer/svn_checkout_tag.sh
@@ -0,0 +1,24 @@
+echo "SVN_SRC=${SVN_SRC}"
+echo "SVN_DEST=${SVN_DEST}"
+
+if [  "X${SVN_SRC}" = "XX" ] ; then
+    echo "SVN_SRC not defined"
+    exit 1
+fi
+if [ "X${SVN_DEST}" = "X" ] ; then
+    echo "SVN_DEST not defined"
+    exit 1
+fi
+ORIGINALDIR=`pwd`
+
+SVN_TAGS="${SVN_SRC}/tags"
+
+
+TAG_LAST=$(svn ls ${SVN_TAGS} | org_desy_grid_virt_sort_release.py  | tail -n 1)
+
+svn co ${SVN_TRUNK}/tags/${TAG_LAST} ${SVN_DEST}
+
+BUILD_SRC=${SVN_DEST}
+export BUILD_SRC
+export SVN_TRUNK
+
