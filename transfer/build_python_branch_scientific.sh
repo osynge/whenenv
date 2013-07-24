f159d0bba72ecddbc5626d4fe188baaa5a57497a
Owen Synge <owen.synge@jaysnest.de>
2013-07-24 19:21:38 +0200

    adding sdist

--- transfer/build_python_branch_scientific.sh
+++ transfer/build_python_branch_scientific.sh
@@ -5,16 +5,24 @@ if [ "X${BUILD_SRC}" = "X" ] ; then
 fi
 ORIGINALDIR=`pwd`
 cd $BUILD_SRC
+
+python setup.py sdist
+for src in $(ls dist/*.tar\.gz | grep -v \.src\.tar\.gz )
+do
+newname=$( echo \${src} | sed -e "s/tar\.gz/rc${BUILD_NUMBER}\.src\.tar\.gz/")
+mv $src $newname
+done
 python setup.py bdist_rpm \
     --release rc${BUILD_NUMBER} \
     --requires  "smimeX509validation hepixvmitrust python-sqlalchemy fetch-crl"
 python setup.py bdist
 architecture=$(arch)
-for src in $(ls dist/*.tar.gz | grep $architecture )
+for src in $(ls dist/*.tar.gz | grep \$architecture )
 do
-  newname=$( echo ${src} | sed -e "s/tar\.gz/rc${BUILD_NUMBER}\.bin\.tar\.gz/")
-  mv $src $newname
+newname=$( echo ${src} | sed -e "s/tar\.gz/rc${BUILD_NUMBER}\.bin\.tar\.gz/")
+mv $src $newname
 done
+
 cd $ORIGINALDIR
 rm -f artifacts.tgz
 tar -zcvf artifacts.tgz build/dist
