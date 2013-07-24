d266602fed7b02c237a2c3839bc66ad06530f62b
Owen Synge <owen.synge@jaysnest.de>
2013-07-24 19:54:23 +0200

    removing ..

--- transfer/build_python_tag_scientific.sh
+++ transfer/build_python_tag_scientific.sh
@@ -9,7 +9,7 @@ cd $BUILD_SRC
 python setup.py sdist
 for src in $(ls dist/*.tar.gz | grep -v .src.tar.gz )
 do
-newname=$( echo ${src} | sed -e "s/tar\.gz/\.src\.tar\.gz/")
+newname=$( echo ${src} | sed -e "s/tar\.gz/src\.tar\.gz/")
 mv $src $newname
 done
 
@@ -17,7 +17,7 @@ python setup.py bdist
 architecture=$(arch)
 for src in $(ls dist/*.tar.gz | grep $architecture )
 do
-newname=$( echo ${src} | sed -e "s/tar\.gz/\.bin\.tar\.gz/")
+newname=$( echo ${src} | sed -e "s/tar\.gz/bin\.tar\.gz/")
 mv $src $newname
 done
 
