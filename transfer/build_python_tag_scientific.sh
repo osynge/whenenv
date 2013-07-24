8206d67059ff78ce2f891229933fdc03b4c97955
Owen Synge <owen.synge@jaysnest.de>
2013-07-24 19:43:52 +0200

    adding sdist

--- transfer/build_python_tag_scientific.sh
+++ transfer/build_python_tag_scientific.sh
@@ -5,8 +5,24 @@ if [ "X${BUILD_SRC}" = "X" ] ; then
 fi
 ORIGINALDIR=`pwd`
 cd $BUILD_SRC
+
+python setup.py sdist
+for src in $(ls dist/*.tar.gz | grep -v .src.tar.gz )
+do
+newname=$( echo ${src} | sed -e "s/tar\.gz/rc${BUILD_NUMBER}\.src\.tar\.gz/")
+mv $src $newname
+done
+
 python setup.py bdist_rpm \
     --requires  "python-sqlalchemy m2crypto python-magic smimeX509validation "
+
+architecture=$(arch)
+for src in $(ls dist/*.tar.gz | grep $architecture )
+do
+newname=$( echo ${src} | sed -e "s/tar\.gz/rc${BUILD_NUMBER}\.bin\.tar\.gz/")
+mv $src $newname
+done
+
 cd $ORIGINALDIR
 rm -f artifacts.tgz
 tar -zcvf artifacts.tgz build/dist
