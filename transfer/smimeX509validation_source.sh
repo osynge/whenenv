97b0c296d63b317f6eb70253b14ec74773cd6968
Owen Synge <owen.synge@jaysnest.de>
2013-07-24 21:51:29 +0200

    adding smimeX509validation

---
+++ transfer/smimeX509validation_source.sh
@@ -0,0 +1,8 @@
+GIT_SRC="git://github.com/hepix-virtualisation/smimeX509validation.git"
+GIT_DEST="build"
+GIT_TAG_FILTER="smimeX509validation-"
+RPM_DEPENDS="m2crypto"
+export GIT_SRC
+export GIT_DEST
+export GIT_TAG_FILTER
+export RPM_DEPENDS
