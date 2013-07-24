15033bc8fe384785e85eb371eb47b1d572d7b031
Owen Synge <owen.synge@jaysnest.de>
2013-07-24 21:18:42 +0200

    adding dirvacua

---
+++ transfer/dirvacua_source.sh
@@ -0,0 +1,8 @@
+GIT_SRC="https://github.com/osynge/dirvacua.git"
+GIT_DEST="build"
+GIT_TAG_FILTER="dirvacua-"
+RPM_DEPENDS=""
+export GIT_SRC
+export GIT_DEST
+export GIT_TAG_FILTER
+export RPM_DEPENDS
