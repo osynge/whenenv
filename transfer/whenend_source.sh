94155d7d9c64b14d7bd2d4810be5240ff2cd1ca1
Owen Synge <owen.synge@jaysnest.de>
2013-08-04 17:46:16 +0200

    made whenenv

---
+++ transfer/whenend_source.sh
@@ -0,0 +1,8 @@
+GIT_SRC="git://git.fritz.box/yokel_jenkins.git"
+GIT_DEST="build"
+GIT_TAG_FILTER="whenenv-"
+RPM_DEPENDS="bash"
+export GIT_SRC
+export GIT_DEST
+export GIT_TAG_FILTER
+export RPM_DEPENDS
