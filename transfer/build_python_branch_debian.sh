185cdbcffe76e69ed0c3370f2895d8d40b1e2dde
Owen Synge <owen.synge@jaysnest.de>
2013-08-04 20:17:40 +0200

    looking better

--- transfer/build_python_branch_debian.sh
+++ transfer/build_python_branch_debian.sh
@@ -6,6 +6,13 @@ fi
 rm -f artifacts.tgz
 ORIGINALDIR=`pwd`
 cd $BUILD_SRC
+python setup.py sdist
+for src in $(ls dist/*.tar.gz | grep -v .src.tar.gz )
+do
+newname=$( echo ${src} | sed -e "s/tar\.gz/src\.tar\.gz/")
+mv $src $newname
+done
+
 python setup.py bdist
 architecture=$(arch)
 for src in $(ls dist/*.tar.gz | grep $architecture )
