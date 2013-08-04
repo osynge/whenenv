91d759060327a4a8420eae205b9865a59d9e95a3
Owen Synge <owen.synge@jaysnest.de>
2013-08-04 12:04:10 +0200

    might work now

--- transfer/test_run_vmcatcher.sh
+++ transfer/test_run_vmcatcher.sh
@@ -1,4 +1,5 @@
-chroot $CHROOT
+CHROOT_SCRIPT=${CHROOT}/script
+cat > ${CHROOT_SCRIPT} <<-EOF
 cd build
 echo `fetch-crl`
 cp /etc/yokel/hepix_tests_keydetails.py keydetails.py
@@ -7,3 +8,8 @@ python test_smimeX509validation.py
 python test_vmlisub_endorser.py
 python test_vmlisub_sub.py
 python test_vmlisub_cache.py
+EOF
+echo xx
+cat ${CHROOT_SCRIPT}
+echo xx
+chroot ${CHROOT} /bin/bash /script
