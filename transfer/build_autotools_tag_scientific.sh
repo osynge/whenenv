e6ef06987744623adef88af930f679281fe61f56
Owen Synge <owen.synge@jaysnest.de>
2013-08-13 01:31:38 +0200

    improved

--- transfer/build_autotools_tag_scientific.sh
+++ transfer/build_autotools_tag_scientific.sh
@@ -14,7 +14,10 @@ cd $BUILD_SRC
 ./autogen.sh 
 ./configure
 make dist
-make install DESTDIR=${ORIGINALDIR}/bdist
+mkdir dist
+mv *.tar.gz dist
+make
+make install DESTDIR=`pwd`/bdist
 # make apidoc
 make rpm
 
@@ -25,14 +28,13 @@ rm -f artifacts.tgz
 
 
 mkdir -p build/dist
-tar -C bdist -zcvf build/dist/${GIT_TAG_LAST}.bin.tar.gz .
+tar -C ${$BUILD_SRC}/bdist -zcvf build/dist/${GIT_TAG_LAST}.bin.tar.gz .
 
 BASEFILE=`ls build/*tar.gz`
 NEWFILE=`echo ${BASEFILE} | sed -e 's/\.tar\.gz/\.src\.tar\.gz/g' | sed -e 's/build/build\/dist/' `
 mv ${BASEFILE} ${NEWFILE}
 
 cp build/RPMS/x86_64/* build/dist
-cp build/SOURCES/* build/dist
 cp build/SRPMS/* build/dist
 
 tar -zcvf artifacts.tgz build/dist
