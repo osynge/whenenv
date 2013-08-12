30ef9b1e2f5243079cee71c9a3ef2f0d3c2b9452
Owen Synge <owen.synge@jaysnest.de>
2013-08-12 23:47:15 +0200

    added libsubprocess

---
+++ transfer/libsubprocess_source.sh
@@ -0,0 +1,12 @@
+GIT_SRC="git://github.com/osynge/libsubprocess.git"
+GIT_DEST="build"
+GIT_TAG_FILTER="libsubprocess-"
+RPM_DEPENDS="rpm"
+REPOSITORY_TYPE="public"
+BUILD_TYPE="autotools"
+export GIT_SRC
+export GIT_DEST
+export GIT_TAG_FILTER
+export RPM_DEPENDS
+export REPOSITORY_TYPE
+export BUILD_TYPE
