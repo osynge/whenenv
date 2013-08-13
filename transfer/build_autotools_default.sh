cb533daa8b260debf6ca30b5edd03347bf8b2fa9
Owen Synge <owen.synge@jaysnest.de>
2013-08-13 03:00:52 +0200

    maybe this works

--- transfer/build_autotools_default.sh
+++ transfer/build_autotools_default.sh
@@ -9,7 +9,6 @@ mv *.tar.gz dist
 make
 make install DESTDIR=`pwd`/bdist
 # make apidoc
-make rpm
 
 # Now we make the artifacts
 
@@ -24,8 +23,6 @@ BASEFILE=`ls build/*tar.gz`
 NEWFILE=`echo ${BASEFILE} | sed -e 's/\.tar\.gz/\.src\.tar\.gz/g' | sed -e 's/build/build\/dist/' `
 mv ${BASEFILE} ${NEWFILE}
 
-cp build/RPMS/x86_64/* build/dist
-cp build/SRPMS/* build/dist
 
 tar -zcvf artifacts.tgz build/dist
 rm -rf $BUILD_SRC
