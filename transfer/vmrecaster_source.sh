16735c01870f3200908a718d9d9a7131d7332c5f
Owen Synge <owen.synge@jaysnest.de>
2013-08-13 02:22:25 +0200

    now are things getting better

--- transfer/vmrecaster_source.sh
+++ transfer/vmrecaster_source.sh
@@ -3,8 +3,10 @@ GIT_DEST="build"
 GIT_TAG_FILTER="vmrecaster_"
 RPM_DEPENDS="bash"
 REPOSITORY_TYPE="private"
+BUILD_TYPE="disttools"
 export GIT_SRC
 export GIT_DEST
 export GIT_TAG_FILTER
 export RPM_DEPENDS
 export REPOSITORY_TYPE
+export BUILD_TYPE
