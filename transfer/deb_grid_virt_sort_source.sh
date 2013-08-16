1218e7f86622f3327090ea180569917c09e3a004
Owen Synge <owen.synge@jaysnest.de>
2013-08-17 01:41:10 +0200

    hopefully stiull working

---
+++ transfer/deb_grid_virt_sort_source.sh
@@ -0,0 +1,11 @@
+GIT_SRC="https://github.com/osynge/deb-grid-virt-sort.git "
+GIT_DEST="build"
+GIT_TAG_FILTER="whenenv-"
+RPM_DEPENDS="bash"
+BUILD_TYPE="gitbuildpackage"
+export GIT_SRC
+export GIT_DEST
+export GIT_TAG_FILTER
+export RPM_DEPENDS
+export REPOSITORY_TYPE
+export BUILD_TYPE
