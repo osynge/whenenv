429ec626a595ede8ca8f02eed742508248ad9772
Owen Synge <owen.synge@jaysnest.de>
2013-08-24 04:55:09 +0200

    correct git uri

--- transfer/docbook_yokel_welcome_source.sh
+++ transfer/docbook_yokel_welcome_source.sh
@@ -1,6 +1,6 @@
-GIT_SRC="https://github.com/hepix-virtualisation/hepix-virtualisation-book.git"
+GIT_SRC="git://git.fritz.box/docbook_yokel_welcome.git"
 GIT_DEST="build"
-GIT_TAG_FILTER="book_"
+GIT_TAG_FILTER="yokel_welcome_"
 export GIT_SRC
 export GIT_DEST
 export GIT_TAG_FILTER
