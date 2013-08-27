5b9ef830df6e311364ca1695559b8f3b375bc47f
Owen Synge <owen.synge@jaysnest.de>
2013-08-27 14:56:15 +0200

    adding vmimagemanager

---
+++ transfer/vmimagemanager_source.sh
@@ -0,0 +1,12 @@
+GIT_SRC="https://github.com/hepix-virtualisation/vmimagemanager"
+GIT_DEST="build"
+GIT_TAG_FILTER="vmimagemanager-"
+RPM_DEPENDS="libvirt-python"
+REPOSITORY_TYPE="public"
+BUILD_TYPE="disttools"
+export GIT_SRC
+export GIT_DEST
+export GIT_TAG_FILTER
+export RPM_DEPENDS
+export REPOSITORY_TYPE
+export BUILD_TYPE
