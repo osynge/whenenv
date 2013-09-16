752e55dade2de86f714ec16ceac9d3502bc6d4db
Owen Synge <owen.synge@jaysnest.de>
2013-09-16 18:43:24 +0200

    start to add inheritance

--- jjobrun/chroot_package_installer_base.py
+++ jjobrun/chroot_package_installer_base.py
@@ -23,110 +23,46 @@ import watcher
 import time
 import json
 import datetime
-import chroot_package_installer_base
+
 
 syncDelay = datetime.timedelta(seconds=100)
 timeoutDelay = datetime.timedelta(seconds=500)
 syncDelayShort = datetime.timedelta(seconds=1)
 timeoutDelayShort = datetime.timedelta(seconds=5)
 
-class ChrootPackageInstallerDebian2(chroot_package_installer_base.ChrootPackageInstallerBase):
+class ChrootPackageInstallerBase(object):
 
     def __init__(self, *args, **kwargs):
-        chroot_package_installer_base.ChrootPackageInstallerBase.__init__(self)
         # we still need these things chrootCmd, env):
-        self.log = logging.getLogger("ChrootPackageInstallerDebian2")
+        self.log = logging.getLogger("ChrootPackageInstallerBase")
         self.chrootCmd = kwargs.get('command', None)
         self.logOut = logging.getLogger("pkg.out")
         self.logErr = logging.getLogger("pkg.err")
-        
-        
+    
+    def logOutput(self,fd,data,args,keys):
+        log = self.log
+        if fd == 0:
+            log = self.logOut
+        if fd == 1:
+            log = self.logErr
+        for line in data.split('\n'):
+            cleanline = line.strip()
+            if len(cleanline) > 0:
+                log.info(cleanline)
         
     def logOutputPkg(self,fd,data,args,keys):
-        lines = data.split('\n')
-        for line in lines:
-            if len(line) == 0:
-                continue
-            if self.waitingOnPromptPkgInstallEnd == True:
-                matches = self.promptPkgInstallEnd.match(line)
-                if matches != None:
-                    self.waitingOnPromptPkgInstallEnd = False
-                    continue
-            if self.waitingOnPromptPkgInstallStart == True:
-                matches = self.promptPkgInstallStart.match(line)
-                if matches != None:
-                    self.waitingOnPromptPkgInstallStart = False
-                    self.waitingOnPromptPkgInstallEnd = True
-                    continue
-                
-            #self.logOutput(fd,data,args,keys)
+        self.log.info("logOutputPkginstall")
+        pass
             
     def logOutputPkgCatUpdate(self,fd,data,args,keys):
-        lines = data.split('\n')
-        foundpackages = set([])
-        deinstalledPackages = set([])
-        for line in lines:
-            cleanline = line.strip()
-            if len(cleanline) == 0:
-                continue
-            if self.waitingOnPromptPkgCatUpdateEnd == True:
-                matches = self.promptPkgCatUpdateEnd.match(line)
-                if matches != None:
-                    self.waitingOnPromptPkgCatUpdateEnd = False
-                    continue
-            if self.waitingOnPromptPkgCatUpdateStart == True:
-                matches = self.promptPkgCatUpdateStart.match(line)
-                if matches != None:
-                    self.waitingOnPromptPkgCatUpdateStart = False
-                    self.waitingOnPromptPkgCatUpdateEnd = True
-                    continue
-            if not cleanline[:15] == '{ "Package" : "':
-               continue
-            fred = json.loads(cleanline)
-            
-            if fred["Status"] ==  'install ok installed':
-                if fred["Package"] == "rpm":
-                    print "adding rpm",fred["Status"]
-                foundpackages.add(str(fred["Package"]))
-                continue
-            if fred["Status"] ==  u'deinstall ok config-files':
-                deinstalledPackages.add(str(fred["Package"]))
-                continue
-            #print fred
-            self.logOutput(fd,data,args,keys)
-        
-        missing = foundpackages.difference(self.PkgCatInstalled)
-        for item in missing:
-            self.PkgCatInstalled.add(item)
-        missing = deinstalledPackages.difference(self.PkgCatInstalled)
-        for item in missing:
-            self.PkgCatDeinstall.add(item)
-        
-        
-        return True
+        self.log.info("logOutputPkginstall")
+        pass
+
+
     def logOutputPkginstall(self,fd,data,args,keys):
-        #self.log.info("logOutputPkginstall")
-        Now = datetime.datetime.now()
-        self.SyncTime = syncDelay + Now
-        self.logOutput(fd,data,args,keys)
-        lines = data.split('\n')
-        foundpackages = set([])
-        deinstalledPackages = set([])
-        for line in lines:
-            cleanline = line.strip()
-            if len(cleanline) == 0:
-                continue
-            if self.waitingOnPromptPkgInstallEnd == True:
-                matches = self.promptPkgInstallEnd.match(line)
-                if matches != None:
-                    self.waitingOnPromptPkgInstallEnd = False
-                    continue
-            if self.waitingOnPromptPkgInstallStart == True:
-                matches = self.promptPkgInstallStart.match(line)
-                if matches != None:
-                    self.waitingOnPromptPkgInstallStart = False
-                    self.waitingOnPromptPkgInstallEnd = True
-                    continue
+        self.log.info("logOutputPkginstall")
+        pass
+        
         
         
         
@@ -187,6 +123,9 @@ class ChrootPackageInstallerDebian2(chroot_package_installer_base.ChrootPackageI
                 break
         self.running.CbDelOnFdRead(self.logOutputPkgCatUpdate)
         return self.PkgCatInstalled
+
+
+
     def installPackage(self,package):  
         Now = datetime.datetime.now()
         self.SyncTime = syncDelay + Now
