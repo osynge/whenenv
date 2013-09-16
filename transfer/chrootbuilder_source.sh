24e666c5014fb0588dfa8aa4c774435aec65a935
Owen Synge <owen.synge@jaysnest.de>
2013-09-16 20:31:33 +0200

    build now

---
+++ transfer/chrootbuilder_source.sh
@@ -0,0 +1,12 @@
+GIT_SRC="git://git.fritz.box/chrootbuilder.git"
+GIT_DEST="build"
+GIT_TAG_FILTER="chrootbuilder_"
+RPM_DEPENDS="bash"
+REPOSITORY_TYPE="private"
+BUILD_TYPE="disttools"
+export GIT_SRC
+export GIT_DEST
+export GIT_TAG_FILTER
+export RPM_DEPENDS
+export REPOSITORY_TYPE
+export BUILD_TYPE
