ad74a522368a9488435ec403b1bbbddc33b86e53
Owen Synge <owen.synge@jaysnest.de>
2015-11-20 01:02:40 +0100

    add error checking

--- transfer/build_autotools_make_package_scientific_6.sh
+++ transfer/build_autotools_make_package_scientific_6.sh
@@ -1,5 +1,41 @@
+if [ "X${DIR_ART_RPMS}" = "X" ] ; then
+echo "DIR_ART_RPMS not defined"
+sleep 1
+exit 1
+fi
+
+if [ "X${DIR_ART_SRPMS}" = "X" ] ; then
+echo "DIR_ART_SRPMS not defined"
+sleep 1
+exit 1
+fi
+
+if [ "X${BUILD_SRC}" = "X" ] ; then
+echo "DIR_ART_SRPMS not defined"
+sleep 1
+exit 1
+fi
+
 ORIGINALDIR=`pwd`
 cd $BUILD_SRC
 make rpm
-cp RPMS/x86_64/* dist
-cp SRPMS/* dist
+
+set +e
+files_art_src=$(ls RPMS/x86_64/*)
+set -e
+if [ "X${files_art_src}" != "X" ] ; then
+for art_src in $files_art_src
+do
+  /usr/bin/rsync -v --ignore-existing ${art_src} ${DIR_ART_RPMS}
+done
+fi
+
+set +e
+files_art_src=$(ls ${DIR_ART_SRC}/*.src.tar.gz)
+set -e
+if [ "X${files_art_src}" != "X" ] ; then
+for art_src in $files_art_src
+do
+  /usr/bin/rsync -v --ignore-existing ${art_src} ${DIR_ART_SRPMS}
+done
+fi
