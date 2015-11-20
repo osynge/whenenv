1a7e25153049fa6faee4ee05a83a763b12aea571
Owen Synge <owen.synge@jaysnest.de>
2015-11-20 01:05:02 +0100

    add binary and sorce to the artifact directory

--- transfer/build_autotools_make.sh
+++ transfer/build_autotools_make.sh
@@ -1,12 +1,29 @@
+if [ "X${DIR_ART_BIN}" = "X" ] ; then
+echo "DIR_ART_RPMS not defined"
+sleep 1
+exit 1
+fi
+
+if [ "X${DIR_ART_SRC}" = "X" ] ; then
+echo "DIR_ART_SRPMS not defined"
+sleep 1
+exit 1
+fi
+
+if [ "X${BUILD_SRC}" = "X" ] ; then
+echo "DIR_ART_SRPMS not defined"
+sleep 1
+exit 1
+fi
+
 ORIGINALDIR=`pwd`
 cd $BUILD_SRC
 make dist
 mkdir dist
 FILENAME=`ls -t *tar.gz | head -n 1`
 NEWNAME=`echo ${FILENAME} | sed -e 's/\.tar\.gz/\.src\.tar\.gz/g'`
-mv $FILENAME dist/$NEWNAME
+mv $FILENAME ${DIR_ART_SRC}/$NEWNAME
 make
-make install DESTDIR=`pwd`/bdist
-mkdir -p dist
-tar -C bdist -zcvf dist/${PRODUCT}_${SRC_VERSION}.$(arch).bin.tar.gz .
+make install DESTDIR=`pwd`/bdist/${PRODUCT}-${SRC_VERSION}
+tar -C bdist -zcvf ${DIR_ART_BIN}/${PRODUCT}_${SRC_VERSION}.$(arch).bin.tar.gz .
 # make apidoc
