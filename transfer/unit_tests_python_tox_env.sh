7fde73f675e05f59c33182285c9f7284dd4bac4e
Owen Synge <owen.synge@jaysnest.de>
2017-03-09 01:26:54 +0100

    Add Missing script file

--- transfer/unit_tests_python_tox_env.sh
+++ transfer/unit_tests_python_tox_env.sh
@@ -1,15 +1,8 @@
 TOXCMD="tox"
-
-if [ "X${WORKSPACE}" = "X" ] ; then
-    WORKSPACE=$HOME
+if [ "X${TOXDIR_PREFIX}" = "X" ] ; then
+    echo "TOXDIR_PREFIX not defined"
+    TOXDIR_PREFIX=${HOME}/workspace/tox
 fi
-
-if [ "X${WORKSPACE}" = "X" ] ; then
-    echo "BUILD_SRC not defined"
-    exit 1
-fi
-
-TOXDIR_PREFIX=${WORKSPACE}/tox
 TOXDIR="${TOXDIR_PREFIX}/executor_${EXECUTOR_NUMBER}_dir"
 export TOXCMD
 export TOXDIR
