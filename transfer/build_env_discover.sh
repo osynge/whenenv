88e1ae5dd74d7dfff3e3e6115cb2b511fd69163a
Owen Synge <owen.synge@jaysnest.de>
2017-04-08 15:52:02 +0200

    add PY_OS_VER_MIN

--- transfer/build_env_discover.sh
+++ transfer/build_env_discover.sh
@@ -1,5 +1,10 @@
 PY_OS=`python -c "import platform; import sys; sys.stdout.write(platform.linux_distribution()[0].strip())"`
 PY_OS_VER_MAJ=`python -c "import platform; import sys; sys.stdout.write(platform.linux_distribution()[1].split('.')[0])"`
+set +e
+PY_OS_VER_MIN=`python -c "import platform; import sys; sys.stdout.write(' '.join(platform.linux_distribution()[1].split('.')[1:]))"`
+edilrc=$?
+set -e
+echo PY_OS_VER_MIN_RC=$edilrc
 
 if [ "X${PY_OS}" = "Xdebian" ] ; then
 PKG_FORMAT="dpkg"
@@ -19,4 +24,5 @@ PKG_FORMAT="None"
 fi
 export PY_OS
 export PY_OS_VER_MAJ
+export PY_OS_VER_MIN
 export PKG_FORMAT
