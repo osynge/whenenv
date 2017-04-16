472b35b98ffe0606b082357e07e9fc890da85bed
Owen Synge <owen.synge@jaysnest.de>
2017-04-16 11:16:59 +0200

    Dont rename things twice bugfix
    With dist tools bdist and sdist tar.gz where being renamed twice
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- transfer/build_python_bdist_rename_development.sh
+++ transfer/build_python_bdist_rename_development.sh
@@ -4,21 +4,30 @@ if [ "X${BUILD_SRC}" = "X" ] ; then
     exit 1
 fi
 
-
 if [  "X${CHECKOUT_DATE}" = "X" ] ; then
     echo "CHECKOUT_DATE not defined"
     sleep 4
     exit 1
 fi
 
-
 cd $BUILD_SRC
 architecture=$(arch)
-for src in $(ls dist/*.tar.gz | grep $architecture | grep -v ${CHECKOUT_DATE} )
+
+for src in $(ls dist/*.tar.gz | grep $architecture )
 do
-newname=$( echo ${src} | sed -e "s/tar\.gz/rc${CHECKOUT_DATE}\.bin\.tar\.gz/")
-mv $src $newname
-if [ "X${DIR_ART_BIN}" != "X" ] ; then
-cp $newname ${DIR_ART_BIN}
-fi
+    newname=$( echo ${src} | sed -e "s/tar\.gz/bin\.tar\.gz/")
+    mv $src $newname
+done
+
+for src in $(ls dist/*bin.tar.gz | grep -v ${CHECKOUT_DATE} )
+do
+    newname=$( echo ${src} | sed -e "s/bin\.tar\.gz/rc${CHECKOUT_DATE}\.bin\.tar\.gz/")
+    mv $src $newname
+done
+
+for src in $(ls dist/*bin.tar.gz | grep $architecture | grep ${CHECKOUT_DATE} )
+do
+    if [ "X${DIR_ART_BIN}" != "X" ] ; then
+        cp ${src} ${DIR_ART_BIN}
+    fi
 done
--- transfer/build_python_sdist_rename_development.sh
+++ transfer/build_python_sdist_rename_development.sh
@@ -2,12 +2,29 @@ if [ "X${BUILD_SRC}" = "X" ] ; then
     echo "BUILD_SRC not defined"
     exit 1
 fi
-cd $BUILD_SRC
-for src in $(ls dist/*.tar.gz | grep -v .src.tar.gz | grep -v ${CHECKOUT_DATE} )
-do
-newname=$( echo ${src} | sed -e "s/tar\.gz/rc${CHECKOUT_DATE}\.src\.tar\.gz/")
-mv $src $newname
-if [ "X${DIR_ART_SRC}" != "X" ] ; then
-cp $newname ${DIR_ART_SRC}
+
+if [  "X${CHECKOUT_DATE}" = "X" ] ; then
+    echo "CHECKOUT_DATE not defined"
+    sleep 4
+    exit 1
 fi
+
+cd $BUILD_SRC
+for src in $(ls dist/*.tar.gz | grep -v .src.tar.gz )
+do
+    newname=$( echo ${src} | sed -e "s/tar\.gz/src\.tar\.gz/")
+    mv $src $newname
+done
+
+for src in $(ls dist/*.src.tar.gz | grep -v ${CHECKOUT_DATE} )
+do
+    newname=$( echo ${src} | sed -e "s/src\.tar\.gz/rc${CHECKOUT_DATE}\.src\.tar\.gz/")
+    mv $src $newname
+done
+
+for src in $(ls dist/*.src.tar.gz | grep ${CHECKOUT_DATE} )
+do
+    if [ "X${DIR_ART_SRC}" != "X" ] ; then
+        cp ${src} ${DIR_ART_SRC}
+    fi
 done
