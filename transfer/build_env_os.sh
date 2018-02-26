ecc4fb68a37931e3b358ec6ccd7120753234ee34
Owen Synge <owen.synge@jaysnest.de>
2018-02-26 23:57:28 +0100

    Improved build_env_os.sh
    This shell script now uses easier to read python
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- transfer/build_env_os.sh
+++ transfer/build_env_os.sh
@@ -1,10 +1,33 @@
-PY_OS=`python -c "import platform; import sys; sys.stdout.write(platform.linux_distribution()[0].strip())"`
-PY_OS_VER_MAJ=`python -c "import platform; import sys; sys.stdout.write(platform.linux_distribution()[1].split('.')[0])"`
-set +e
-PY_OS_VER_MIN=`python -c "import platform; import sys; sys.stdout.write(' '.join(platform.linux_distribution()[1].split('.')[1:]))"`
-edilrc=$?
-set -e
-echo PY_OS_VER_MIN_RC=$edilrc
+#!/bin/bash
+
+
+function py_os_var {
+PY_ACTION="$1" python - <<END
+import platform
+import re
+import sys
+import os
+action = os.environ['PY_ACTION']
+py_os = platform.linux_distribution()[0]
+py_os_ver_maj = 0
+py_os_ver_min = 0
+version_raw_split = re.split('[.\/]', platform.linux_distribution()[1])
+if len(version_raw_split) == 2:
+    py_os_ver_maj =  version_raw_split[0]
+    py_os_ver_min =  version_raw_split[1]
+if action == "py_os":
+    sys.stdout.write(py_os)
+if action == "py_os_ver_maj":
+    sys.stdout.write(py_os_ver_maj)
+if action == "py_os_ver_min":
+    sys.stdout.write(py_os_ver_min)
+END
+}
+
+
+PY_OS=`py_os_var py_os`
+PY_OS_VER_MAJ=`py_os_var py_os_ver_maj`
+PY_OS_VER_MIN=`py_os_var py_os_ver_min`
 
 if [ "X${PY_OS}" = "Xdebian" ] ; then
 PKG_FORMAT="dpkg"
@@ -22,7 +45,12 @@ fi
 if [ "X${PKG_FORMAT}" = "X" ] ; then
 PKG_FORMAT="None"
 fi
+#echo PY_OS=$PY_OS
+#echo PY_OS_VER_MAJ=$PY_OS_VER_MAJ
+#echo PY_OS=$PY_OS_VER_MIN
+#echo PY_OS=$PKG_FORMAT
 export PY_OS
 export PY_OS_VER_MAJ
 export PY_OS_VER_MIN
 export PKG_FORMAT
+
