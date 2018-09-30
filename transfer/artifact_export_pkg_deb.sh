f4fbf1b9b9776b4eb8d5915276b57e39b676377a
Owen Synge <owen.synge@jaysnest.de>
2018-09-30 02:47:23 +0200

    Generate dpkg insecure repos
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- transfer/artifact_export_pkg_deb.sh
+++ transfer/artifact_export_pkg_deb.sh
@@ -15,9 +15,17 @@ mkdir -p ${DIR_EXPORT_DPKG}
 set +e
 files_art_bin=$(ls ${DIR_ART_DPKG}/*.deb)
 set -e
+loopcounter=0
+RSYNC_LOG_FILE=rsync_log_file.log
 if [ "X${files_art_bin}" != "X" ] ; then
 for art_bin in $files_art_bin
 do
-/usr/bin/rsync -v --ignore-existing ${art_bin} ${DIR_EXPORT_DPKG}
+rm -f ${RSYNC_LOG_FILE}
+touch ${RSYNC_LOG_FILE}
+/usr/bin/rsync  --log-file=${RSYNC_LOG_FILE}   -v --ignore-existing ${art_bin} ${DIR_EXPORT_DPKG}
+LOGLEN=`wc -l ${RSYNC_LOG_FILE} | cut -d' ' -f1`
+echo LOGLEN=${LOGLEN}
 done
+# Publish packages
+dpkg-scanpackages ${DIR_EXPORT_DPKG} | gzip > ${DIR_EXPORT_DPKG}/Packages.gz
 fi
